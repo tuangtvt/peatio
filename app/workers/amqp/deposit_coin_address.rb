fb0ed76d27e474f2b66d5f362436a60bb35389f3
tuangtvt <34855403+tuangtvt@users.noreply.github.com>
2019-12-18 17:10:51 +0700

    Update deposit_coin_address.rb

--- app/workers/amqp/deposit_coin_address.rb
+++ app/workers/amqp/deposit_coin_address.rb
@@ -5,13 +5,15 @@ module Workers
   module AMQP
     class DepositCoinAddress < Base
       def process(payload)
+        Rails.logger.info 'step 1'
         payload.symbolize_keys!
-
+        Rails.logger.info 'step 2'
         acc = Account.find_by_id(payload[:account_id])
         return unless acc
         return unless acc.currency.coin?
-
+        Rails.logger.info 'step 3'
         wallet = Wallet.active.deposit.find_by(currency_id: acc.currency_id)
+        Rails.logger.info 'step 5'
         unless wallet
           Rails.logger.warn do
             "Unable to generate deposit address."\
@@ -19,18 +21,19 @@ module Workers
           end
           return
         end
-
+        Rails.logger.info 'step 5'
         wallet_service = WalletService.new(wallet)
-
+        Rails.logger.info 'step 7'
         acc.payment_address.tap do |pa|
           pa.with_lock do
             next if pa.address.present?
-
+            Rails.logger.info 'step 8'
             result = wallet_service.create_address!(acc)
-
+            Rails.logger.info 'step 9'
             pa.update!(address: result[:address],
                       secret:  result[:secret],
                       details: result.fetch(:details, {}).merge(pa.details))
+            Rails.logger.info 'step 10'
           end
 
           # Enqueue address generation again if address is not provided.
